9c987f77ea63b51f68e6c2400fe0d64fbcfbe9e1
Maciek <maciek@Jakubs-Server.local>
2012-05-31 17:53:48 +0200

    commit

---
+++ PartyJackpot/List.h
@@ -0,0 +1,13 @@
+//
+//  List.h
+//  PartyJackpot
+//
+//  Created by Maciek on 5/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface List : UIViewController
+
+@end
---
+++ PartyJackpot/List.m
@@ -0,0 +1,43 @@
+//
+//  List.m
+//  PartyJackpot
+//
+//  Created by Maciek on 5/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "List.h"
+
+@interface List ()
+
+@end
+
+@implementation List
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
